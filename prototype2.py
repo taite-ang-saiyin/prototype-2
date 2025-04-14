f4f30d587f0f6a51104fe635b12496945ca4b7c5
Htet Aung Shine <114334766+taite-ang-saiyin@users.noreply.github.com>
2025-04-14 22:45:43 +0630

    Update prototype2.py

--- prototype2.py
+++ prototype2.py
@@ -18,10 +18,13 @@ from langchain_core.output_parsers import StrOutputParser
 import openai
 import logging
 from langchain.schema import SystemMessage,HumanMessage
-from openai import OpenAI
-from fastapi import FastAPI
 import uvicorn
-
+from openai import OpenAI
+from fastapi import FastAPI,HTTPException
+import weaviate
+from weaviate.classes.init import Auth
+from langchain_openai import ChatOpenAI
+from typing import List, Dict
 app=FastAPI()
 
 pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
@@ -109,30 +112,97 @@ def gpt_function(prompt_text:str)->str:
   )
   return completion.choices[0].message.content
 
-embedding_model=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")  
-index=pc.Index('new-hybrid-index')
-vectorstore = PineconeVectorStore(index_name='new-hybrid-index', embedding=embedding_model, namespace="final-note-namespace")
-
-retriever1 = RunnableLambda(lambda query: vectorstore.similarity_search(str(query)))
-
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY")
-)
-llm=RunnableLambda(gpt_function)
-prompt = ChatPromptTemplate.from_template(
-    "You are a teacher assistant in charge of making notes that would be used to teach students of Primary level in Singapore Science. Ensure your notes follow a good format that would introduce the topic from foundation up. You are to strictly use the context provided to you and answer the questions and not miss out any details. Think and generate like a teacher conducting a class for Primary school kids.\n\ncontext:{context}\n\nquestoin:{question}"
-)
-
-rag_chain = (
-    {"context": retriever1 | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+def getnoteresponse(user_input):
+    embedding_model=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")  
+    index=pc.Index('new-hybrid-index')
+    vectorstore = PineconeVectorStore(index_name='new-hybrid-index', embedding=embedding_model, namespace="final-note-namespace")
+    
+    retriever1 = RunnableLambda(lambda query: vectorstore.similarity_search(str(query)))
+    
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY")
+    )
+    llm=RunnableLambda(gpt_function)
+    prompt = ChatPromptTemplate.from_template(
+        "You are a teacher assistant in charge of making notes that would be used to teach students of Primary level in Singapore Science. Ensure your notes follow a good format that would introduce the topic from foundation up. You are to strictly use the context provided to you and answer the questions and not miss out any details. Think and generate like a teacher conducting a class for Primary school kids.\n\ncontext:{context}\n\nquestoin:{question}"
+    )
+    
+    rag_chain = (
+        {"context": retriever1 | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    response = rag_chain.invoke(user_input)
+    return response
+    
 @app.get("/output/{input_msg}")
 def get_result(input_msg:str):
-    response = rag_chain.invoke(input_msg)
+    response = getnoteresponse(input_msg)
     return {"message":response}
+
+class QuestionRequest(BaseModel):
+    level : str
+    difficulty : str 
+    subject : str
+
+def retrieve_notes_from_weaviate(query: str) -> List[Dict]:
+    client = weaviate.connect_to_weaviate_cloud(
+        cluster_url="ggw6eupshybj5ol0gqa8w.c0.asia-southeast1.gcp.weaviate.cloud",
+        auth_credentials=Auth.api_key(api_key="AzOaydnWlANzR8mAioN8HfGHRgreaLHRtEkb"),
+        headers={"X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"]}
+    )
+
+    science_questions = client.collections.get("ScienceQuestion")
+
+    response = science_questions.query.hybrid(
+        query=query,
+        limit=30,
+        return_properties=["question", "subject", "type", "level", "topic", "difficulty", "year", "school"]
+    )
+
+    retrieved_data = [obj.properties for obj in response.objects]
+    client.close()
+
+    return retrieved_data
+
+def debug_context(x):
+    print("🔎 Retrieved Context:\n")
+    print(x)
+    return x
+
+def getQuizResponse(level,difficulty,subject):
+    retriever1 = RunnableLambda(
+        lambda query: retrieve_notes_from_weaviate(query)
+    )
+    
+    debug_node = RunnableLambda(debug_context)
+    client = OpenAI(
+      api_key=os.environ["OPENAI_API_KEY"]
+    )
+    difficulty="hard"
+    input_sen=f"give me 30 {difficulty} mcqs on {subject} for {level} level"
+    llm=RunnableLambda(gpt_function)
+    prompt = ChatPromptTemplate.from_template(
+        "You are a teacher assistant in charge of giving questions that would be used to teach students of Primary level in Singapore Science. Ensure your questions follow a good format that would introduce the topic from foundation up. You are to strictly use the context provided to you and answer the questions and not miss out any details. Think and generate like a teacher conducting a class for Primary school kids.Don't include introduction or conclusion and just give only the extracted questions from the context.\n\ncontext:{context}\n\nquestoin:{question}"
+    )
+    rag_chain = (
+        {"context": retriever1 | format_docs, "question": RunnablePassthrough()}
+        | debug_node
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    result=rag_chain.invoke(iput_sen)
+    return result
+@app.post("/generate")
+async def generate_mcqs(request: QuestionRequest):
+    try:
+        result=getQuizResponse(request.level,request.difficulty,request.subject)
+        return {"result":result}
+    except Exception as e:
+        raise HTTPException (status_code=500,detail=str(e))
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 4000))  # ✅ Render expects port 10000
     uvicorn.run(app, host="0.0.0.0", port=port)     
